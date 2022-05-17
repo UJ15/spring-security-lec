18b4ab5c6678bb739efe8a45f2abf222e4c18ce1
UJ15 <dbwogml15@naver.com>
2022-05-17 23:32:34 +0900

    2일차

--- spring-sec/src/main/java/com/kdt/prmgs/springsec/config/WebSecurityConfigure.java
+++ spring-sec/src/main/java/com/kdt/prmgs/springsec/config/WebSecurityConfigure.java
@@ -50,6 +50,9 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .and()
             .rememberMe()
                 .rememberMeParameter("remember-me")
-                .tokenValiditySeconds(300);
+                .tokenValiditySeconds(300)
+                .and()
+            .requiresChannel()
+                .anyRequest().requiresSecure();
     }
 }
